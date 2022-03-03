dab4fe785929cb02ecf2b3fc5048e758ce85a16a
Muanya <nwokejichimuanya@gmail.com>
2022-03-03 05:30:16 +0100

    test3 commit

--- projAPI/views.py
+++ projAPI/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 from rest_framework import viewsets
 
 
+class Testing:
+    a = 5
+    b = 8
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        print(self.a)
+
+
 class ApiView(viewsets.ViewSet):
-    pass
+    @staticmethod
+    def trial():
+        print('test')
